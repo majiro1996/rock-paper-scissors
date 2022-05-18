70c41793831d91e8399a1923661108325903c1c7
Majiro <majiro1996@gmail.com>
2022-05-18 18:56:30 +0200

    Add function for five rounds of game, keep score and call a winner. Fix syntax of singleRound function.

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-//funtion for random Rock, Paper or Scissors
+//score
+let playerScore=0
+let computerScore=0
+let playerSelection
+//function for random Rock, Paper or Scissors
 function computerPlay(){
     let options=["rock","paper","scissors"];
     let play=options[Math.floor(Math.random()*options.length)];
     return play
 }
-console.log("computer:"+computerPlay());
 //function for a single round of the game
-function round(playerSelection,computerSelection){ 
-let player=playerSelection.toLowerCase()
-if (player === computerSelection){return "tie"}
-else if (player==="rock" & computerSelection==="paper"){return "You Lose, Paper beats Rock"}
-else if (player==="rock" & computerSelection==="scissors"){return "You Win, Rock beats Scissors"}
-else if (player==="paper" & computerSelection==="scissors"){return "You Lose, Scissors beats Paper"}
-else if (player==="paper" & computerSelection==="rock"){return "You Win, Paper beats Rock"}
-else if (player==="scissors" & computerSelection==="rock"){return "You Lose, Rock beats Scissors"}
-else if (player==="scissors" & computerSelection==="paper"){return "You Win, Scissors beats Paper"}
-else "That item is not in the game!"
+function singleRound(playerSelection,computerSelection)
+{ 
+    let player=playerSelection.toLowerCase()
+    if (player === computerSelection){return "tie"}
+    else if (player==="rock" && computerSelection==="paper")
+    {return "You Lose, Paper beats Rock. "+"Computer score: "+ ++computerScore}
+    else if (player==="rock" && computerSelection==="scissors")
+    {return "You Win, Rock beats Scissors. "+"Player score: "+ ++playerScore}
+    else if (player==="paper" && computerSelection==="scissors")
+    {return "You Lose, Scissors beats Paper. "+"Computer score: "+ ++computerScore}
+    else if (player==="paper" && computerSelection==="rock")
+    {return +"You Win, Paper beats Rock. "+"Player score: "+ ++playerScore}
+    else if (player==="scissors" && computerSelection==="rock")
+    {return "You Lose, Rock beats Scissors. "+"Computer score: "+ ++computerScore}
+    else if (player==="scissors" && computerSelection==="paper")
+    {return "You Win, Scissors beats Paper. "+"Player score: "+ ++playerScore}
+    else {return "That item is not in the game!"}
 }
-
+//funcion for a game of 5 rounds
+function game()
+{ 
+    for (let i=0;i<5;i++)
+    {  
+        playerSelection=prompt("choose");
+        console.log(singleRound(playerSelection,computerPlay()))
+        if (i===4 && playerScore>computerScore)
+        {return console.log("YOU WIN!"+ playerScore+"-"+computerScore)}
+        else if (i===4 && playerScore<computerScore)
+        {return console.log("YOU LOSE!"+ playerScore+"-"+computerScore)}       
+    }
+}
+game()
